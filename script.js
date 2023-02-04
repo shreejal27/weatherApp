1b89dc8534ab038ad54e04190582647665dbb2dd
Shreejal Maharjan <shreejal27@gmail.com>
2023-02-04 17:27:18 +0545

    added fetched data to html page

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 function getValue(){
-    const apiKey = bd7474449235b2bf05180173b4c6fa51;
     var city = document.getElementById('city').value;
-    console.log(city);
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const apiKey = "bd7474449235b2bf05180173b4c6fa51";
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey;
     
     fetch(apiUrl)
       .then(response => response.json())
       .then(data => {
+        const name = city;
+        const icon = data.weather[0].icon;
+        const description = data.weather[0].description;
         const temperature = data.main.temp;
         const humidity = data.main.humidity;
-        const windSpeed = data.wind.speed;
-        const weather = data.weather[0].description;
+        const windspeed = data.wind.speed;
     
-        document.getElementById('temperature').innerHTML = `Temperature: ${temperature}`;
-        document.getElementById('humidity').innerHTML = `Humidity: ${humidity}`;
-        document.getElementById('wind-speed').innerHTML = `Wind Speed: ${windSpeed}`;
-        document.getElementById('weather').innerHTML = `Weather: ${weather}`;
+        console.log(name,icon, description,temperature,humidity, windspeed);
+        document.getElementById('temperature').innerHTML = "Weather in " + name;
+        document.getElementById('title').innerHTML =  temperature;
+        document.getElementById('info').innerHTML =  icon + description;
+        document.getElementById('humidity').innerHTML = "Humidity: " + humidity;
+        document.getElementById('wind').innerHTML = "Wind speed: " + windspeed;
+ 
       })
       .catch(error => console.error(error));
 
